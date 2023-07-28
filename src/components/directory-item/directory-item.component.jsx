d729c4fd4b1d12c47cfc525d52511cc09411a450
codanMartin <codanmartin_25@gmail.com>
2023-07-28 15:44:19 +0300

    before host

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,21 +1,25 @@
 import { BackgroundImage, Body, DirectoryItemContainer } from "./directory-item.styles.js";
 
+import { useNavigate } from "react-router-dom";
+
 import { Link } from "react-router-dom";
 
 const DirectoryItem = ({ category }) => {
   const { imageUrl, title } = category;
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(`shop/${title.toLowerCase()}`);
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
       <Body>
-        <Link to={`/shop/${title.toLowerCase()}`}>
-          <h2>{title}</h2>
-          <p>Shop Now</p>
-        </Link>
+        <h2>{title}</h2>
+        <p>Shop Now</p>
       </Body>
     </DirectoryItemContainer>
   );
