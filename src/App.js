30616afd3692ba479075a1c2a7f40dd0480240de
codanMartin <codanmartin_25@gmail.com>
2023-07-28 09:11:55 +0300

    back-up save

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./routes/home/Home";
-import Navigation from "./routes/navigation/Navigation";
-import Authentification from "./routes/authentification/Authentication.jsx";
-import Shop from "./routes/shop/Shop.jsx";
-import Checkout from "./routes/checkout/Checkout";
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index={true} element={<Home />} />
-        <Route path="shop" element={<Shop />} />
-        <Route path="auth" element={<Authentification />} />
-        <Route path="checkout" element={<Checkout />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop/*' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
